6057660ce90f4e6aca1ebaf36be2aaff4def40c6
ismakh9 <122399506+ismakh9@users.noreply.github.com>
2025-01-06 13:38:48 -0500

    Update eslint

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,8 @@ import { navLinks } from "@/constants"
 // import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
-
-const Home = async ({ searchParams }: SearchParamProps) => {
+// { searchParams }: SearchParamProps
+const Home = async () => {
   // const page = Number(searchParams?.page) || 1;
   // const searchQuery = (searchParams?.query as string) || '';
 
