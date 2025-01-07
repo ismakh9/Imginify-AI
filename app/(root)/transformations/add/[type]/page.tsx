7dc7e2ec022e5e64d9771a5f7a1c1107520314d9
ismakh9 <122399506+ismakh9@users.noreply.github.com>
2025-01-07 17:29:21 -0500

    rolled back

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,42 +1,45 @@
-import Header from '@/components/shared/Header';
-import TransformationForm from '@/components/shared/TransformationForm';
-import { transformationTypes } from '@/constants';
-import { getUserById } from '@/lib/actions/user.actions';
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
+// import Header from '@/components/shared/Header'
+// import TransformationForm from '@/components/shared/TransformationForm';
+// import { transformationTypes } from '@/constants'
+// import { getUserById } from '@/lib/actions/user.actions';
+// import { auth } from '@clerk/nextjs/server';
+// import { redirect } from 'next/navigation';
 
-type SearchParamProps = {
-  params: {
-    type: keyof typeof transformationTypes;
-  };
-};
+// const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+//   const { userId } = await auth();
+//   const transformation = transformationTypes[type];
 
-const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
-  const { userId } = await auth();
-  if (!userId) {
-    redirect('/sign-in');
-  }
+//   if(!userId) redirect('/sign-in')
 
-  const transformation = transformationTypes[type];
-  const user = await getUserById(userId);
+//   const user = await getUserById(userId);
 
-  return (
-    <>
-      <Header 
-        title={transformation.title}
-        subtitle={transformation.subTitle}
-      />
+//   return (
+//     <>
+//       <Header 
+//         title={transformation.title}
+//         subtitle={transformation.subTitle}
+//       />
     
-      <section className="mt-10">
-        <TransformationForm 
-          action="Add"
-          userId={user._id}
-          type={transformation.type as keyof typeof transformationTypes}
-          creditBalance={user.creditBalance}
-        />
-      </section>
-    </>
-  );
-};
+//       <section className="mt-10">
+//         <TransformationForm 
+//           action="Add"
+//           userId={user._id}
+//           type={transformation.type as TransformationTypeKey}
+//           creditBalance={user.creditBalance}
+//         />
+//       </section>
+//     </>
+//   )
+// }
 
-export default AddTransformationTypePage;
+// export default AddTransformationTypePage
+
+import React from 'react'
+
+const AddTransformationTypePage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default AddTransformationTypePage
